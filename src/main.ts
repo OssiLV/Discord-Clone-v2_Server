3125732d30730dbf39471cd0fc97a5cd888ab1cc
OssiLV <vohoanganhtruong@gmail.com>
2023-10-03 17:28:53 +0700

    Change URL

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,12 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-    const port = 5174;
     const app = await NestFactory.create(AppModule, {
         cors: true,
     });
-    // app.setGlobalPrefix("api");
+    app.setGlobalPrefix("api");
     await app.listen(process.env.PORT || 5174);
 
-    console.log(`App listening on: http://localhost:${process.env.PORT}/api`);
+    console.log(`App listening on: ${process.env.PORT}`);
 }
 bootstrap();
